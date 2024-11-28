56edf1f08001d4f8c5decc27d5aa4a2a5c14d460
luiss07 <luigi.delleva@gmail.com>
2024-11-28 09:53:31 +0100

    derive PartialEq

--- crates/wg_packet/src/packet.rs
+++ crates/wg_packet/src/packet.rs
@@ -2,14 +2,14 @@ use crate::{FloodRequest, FloodResponse};
 use wg_network::{NodeId, SourceRoutingHeader};
 
 // Is atomic unit to be sent
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Packet {
     pub pack_type: PacketType,
     pub routing_header: SourceRoutingHeader,
     pub session_id: u64,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum PacketType {
     MsgFragment(Fragment),
     Nack(Nack),
@@ -18,25 +18,25 @@ pub enum PacketType {
     FloodResponse(FloodResponse),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Nack {
     pub fragment_index: u64,
     pub nack_type: NackType,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum NackType {
     ErrorInRouting(NodeId), // contains id of not neighbor
     DestinationIsDrone,
     Dropped,
     UnexpectedRecipient(NodeId),
 }
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Ack {
     pub fragment_index: u64,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Fragment {
     pub fragment_index: u64,
     pub total_n_fragments: u64,
